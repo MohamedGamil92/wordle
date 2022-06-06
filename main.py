f17b716cf2a4f80f2a20f1147eb49a20ea23f9d2
Mohazerr <mohazerr@Pirate.local>
2022-06-06 19:25:54 +0200

    dictionnaireen.txt

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import random
 lang = "0"
 print("Which language ?")
 while lang not in ("1", "2", "3"):
-    print("[1] English | [2] Francais | [3] TESTS ")
+    print("[1] English | [2] Francais")
     lang = input()
     if lang == "1": 
-        with open('dictionnaire.txt') as f:
+        with open('dictionnaireen.txt') as f:
             dictio = [line.rstrip('\n') for line in f]
     elif lang == "2":
         with open('dictionnairefr.txt') as f:
@@ -15,39 +15,42 @@ while lang not in ("1", "2", "3"):
             dictio = [line.rstrip('\n') for line in f]
     else:
         print("pine ta mams on a dit 1 ou 2: ")
+print("Bienvenu sur le wordle !\nentrez 'help' pour voir les lettres non utilisées\nentrez 'show' pour voir la réponse")
 dictio = [each_string.lower() for each_string in dictio]
 answer = random.choice(dictio)
 replist = list(answer)
-tentative = input("Mot ? ")
-tentative = tentative.lower()
-esslist = list(tentative)
+tentative = ""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 nombre = 1 
 lemot = ['_','_','_','_','_']
 orange = []
 while tentative != answer:
-    if tentative == "zzzzz":
+    tentative = input("Go: ")
+    tentative = tentative.lower()
+    esslist = list(tentative)
+    if tentative == "help": 
+        print(alphabet)
+    elif tentative == "show":
         print(answer)
-        tentative = input("Tricheur ! ")
-        tentative = tentative.lower()
-        esslist = list(tentative)
     elif tentative in dictio:
         print('Tentative n°%d' %nombre)
         nombre = nombre + 1 
         for i in range(5):
+            if esslist[i] in alphabet:
+                alphabet.remove(esslist[i])
             if esslist[i]==replist[i]:
                 lemot[i] = esslist[i]
+                if lemot[i] not in orange:
+                    orange.append(replist[i])
             else:
                 for n in range(5):
+                    if esslist[i] in alphabet:
+                        alphabet.remove(esslist[i])
                     if esslist[i]==replist[n]:
                         if replist[n] not in orange:
                             orange.append(replist[n])
         print(*lemot, sep="")
         print(*orange, sep=", ")
-        tentative = input("Nouvelle tentative ? ")
-        tentative = tentative.lower()
-        esslist = list(tentative)
     else:
-        tentative = input("pas bon mot! ")
-        tentative = tentative.lower()
-        esslist = list(tentative)
+        print("pas bon mot! ")
 print("GG ! il t'a fallu",nombre,"essais")
